9c27e82266192c9afb4cf05bf555f2f045dba0f2
Tateishi Tadatoshi <ishio39@gmail.com>
2021-10-04 19:50:19 +0900

    add acct.py

---
+++ acct.py
@@ -0,0 +1,69 @@
+import numpy as np
+import pandas as pd
+
+
+def read_csv(filename):
+    names ='date sdate payee account currency amount flag memo'.split()
+    df = pd.read_csv(filename, names=names, na_filter=False)
+    df['date_t'] = pd.to_datetime(df['date'])
+    df = df.sort_values(['date', 'amount'], ascending=[True, False])
+    return df
+
+def df_jpy(df):
+    return df.loc[df['currency'] == '']
+
+def df_currency(df, currency=''):
+    return df.loc[df['currency'] == currency]
+
+def grid_data(df, year):
+    df_y = df_jpy(df)
+    df_y = df_y.loc[df_y['date_t'].dt.year == year]
+    df_p = pd.pivot_table(df_y,
+                          values='amount',
+                          index='account',
+                          columns=df['date_t'].dt.month,
+                          aggfunc=np.sum,
+                          fill_value=0).reset_index()
+    df_p['sum'] = df_p.sum(axis=1)
+
+    return df_p.rename(columns={k: f'm{k:02}' for k in range(1,13)})
+
+def grid_years(df):
+    df_y = df_jpy(df)
+#    df_y = df_y.loc[df_y['payee'] == 'amazon']
+    df_y = df_y.loc[(df['date_t'].dt.year >= 2016) & (df['date_t'].dt.year <= 2021)]
+    df_p = pd.pivot_table(df_y,
+                          values='amount',
+                          index='account',
+                          columns=df['date_t'].dt.year,
+                          aggfunc=np.sum,
+                          fill_value=0).reset_index()
+    df_p['sum'] = df_p.sum(axis=1)
+
+    columns = {t: f'y{t:04}' for t in df_p.columns if isinstance(t, int)}
+    return df_p.rename(columns=columns)
+
+def grid_payee(df, account):
+    df_jp = df_jpy(df)
+    df_jp = df_jp.loc[(df_jp['date_t'].dt.year >= 2016) & (df_jp['date_t'].dt.year <= 2021)]
+    df_acct = df_jp.loc[df_jp['account'] == account]
+    df_grid = pd.pivot_table(df_acct,
+                             values='amount',
+                             index='payee',
+                             columns=df_acct['date_t'].dt.year,
+                             aggfunc=np.sum,
+                             fill_value=0).reset_index()
+    df_grid['sum'] = df_grid.sum(axis=1)
+    columns = {t: f'y{t:04}' for t in df_grid.columns if isinstance(t, int)}
+    return df_grid.rename(columns=columns)
+
+def read_accounts_csv(filename):
+    names ='number name'.split()
+    df = pd.read_csv(filename, names=names, na_filter=False)
+    return df
+
+def account_by_number(df, number):
+    return df.loc[df['number'] == number]['name'].values[0]
+
+def account_by_name(df, name):
+    return df.loc[df['name'] == name]['number'].values[0]
--- server.py
+++ server.py
@@ -2,9 +2,25 @@ from fastapi import FastAPI, Request, Response
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.middleware.cors import CORSMiddleware
 
+import acct
+
+DATAFILE = 'site/data/ledger.csv'
+ACCTFILE = 'site/data/account.csv'
+
+origins = [
+    '*',
+    ]
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+    )
 
 app.mount('/static', StaticFiles(directory='static'), name='static')
 
@@ -97,4 +113,27 @@ async def data_month(response: Response):
     names = 'id value name'.split()
     df = pd.read_csv('data/month.csv', header=None, names=names)
     response.headers['Cache-Control'] = 'no-cache, no-store'
+    return df.to_dict(orient='records')
+
+
+def read_csv(datafile=DATAFILE):
+    return acct.read_csv(datafile)
+
+
+def read_acct_csv(datafile=ACCTFILE):
+    return acct.read_accounts_csv(datafile)
+
+
+@app.get('/api/v1/ledger')
+async def api_ledger(response: Response):
+    df = read_csv()
+    account = '資産:現金:家計財布'
+    data = df.loc[df['account']==account]
+    data['cum'] = data['amount'].cumsum()
+    return data.to_dict(orient='records')
+
+
+@app.get('/api/v1/accounts')
+async def api_accounts(response: Response):
+    df = read_acct_csv()
     return df.to_dict(orient='records')
